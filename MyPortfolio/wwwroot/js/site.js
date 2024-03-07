002491ad06b39abba767f166db4b9abe22187935
Mark Carter <magcarter@hotmail.co.uk>
2024-03-07 15:09:04 +0000

    Portfolio Working

--- MyPortfolio/wwwroot/js/site.js
+++ MyPortfolio/wwwroot/js/site.js
@@ -4,7 +4,7 @@
 // Write your JavaScript code
 
 /*------------------------------------
-	My Portfolio Filter
+    My Portfolio Filter
 -------------------------------------- */
 
 
@@ -15,75 +15,102 @@ const projects = document.querySelectorAll(".project");
 
 //Contact Form Ajax
 document.forms[0].onsubmit = (event) => { //TODO - as part of making the contact form a 'component' refer specifically to contact form not just form[0] 
-	event.preventDefault();
-	
-	let formData = new FormData(document.forms[0]);
-	let elementTarget = event.currentTarget;
-	let targetModalSuffix = elementTarget.getAttribute("data-modalSuffix");
-	let targetModal = new bootstrap.Modal(document.getElementById('modal-' + targetModalSuffix), { keyboard: false })
-	let targetModalMessage = document.getElementById('modal-body-' + targetModalSuffix)
-	let targetModalButton = document.getElementById('modal-button-' + targetModalSuffix)
-	
-	targetModalMessage.innerHTML = "Sending message...";
-	targetModalButton.style = "display: none";
+    event.preventDefault();
 
-	targetModal.show()
-	fetch('?handler=SendMessage', {
-		method: 'post',
-		body: new URLSearchParams(formData)
-	})
-		.then((response) => response.text())
-		.then((result) => {
-			if (result == "Success") {
-				targetModalMessage.innerHTML = "Message Sent Successfully!";
-				resetContactForm;
-				
-			} else {
-				targetModalMessage.innerHTML = "Failed! - Please try again later or email me directly on magcarter@hotmail.co.uk";
-			};
+    let formData = new FormData(document.forms[0]);
+    let elementTarget = event.currentTarget;
+    let targetModalSuffix = elementTarget.getAttribute("data-modalSuffix");
+    let targetModal = new bootstrap.Modal(document.getElementById('modal-' + targetModalSuffix), { keyboard: false })
+    let targetModalMessage = document.getElementById('modal-body-' + targetModalSuffix)
+    let targetModalButton = document.getElementById('modal-button-' + targetModalSuffix)
 
-			targetModalButton.style = "display: block";
-		});
+    targetModalMessage.innerHTML = "Sending message...";
+    targetModalButton.style = "display: none";
+
+    targetModal.show()
+    fetch('?handler=SendMessage', {
+        method: 'post',
+        body: new URLSearchParams(formData)
+    })
+        .then((response) => response.text())
+        .then((result) => {
+            if (result == "Success") {
+                targetModalMessage.innerHTML = "Message Sent Successfully!";
+                resetContactForm;
+
+            } else {
+                targetModalMessage.innerHTML = "Failed! - Please try again later or email me directly on magcarter@hotmail.co.uk";
+            };
+
+            targetModalButton.style = "display: block";
+        });
 };
 
 
 function resetContactForm() {
-	var contactMessage = document.getElementById("contact_message")
-	contactMessage.value = '';
-	contactMessage.setAttribute("placeholder", "Send me another message....")	
+    var contactMessage = document.getElementById("contact_message")
+    contactMessage.value = '';
+    contactMessage.setAttribute("placeholder", "Send me another message....")
 }
 
 
 $(window).on('load', function () {
-	$(".portfolio-menu").find("a").on("click", projectsFilterClick);
-	
+    $(".portfolio-menu").find("a").on("click", projectsFilterClick);
+
 });
 
 function projectsFilterClick(event) {
-	event.preventDefault();
-	const element = event.currentTarget
+    event.preventDefault();
+    const element = event.currentTarget
 
-	currentProjectsFilter = element.getAttribute("data-filter")
+    currentProjectsFilter = element.getAttribute("data-filter")
 
-	updateFilterMenu()
+    updateFilterMenu()
 
-	updateProjectsFilter()
+    updateProjectsFilter()
 }
 
 
 function updateFilterMenu() {
-	projectsFilterMenu.forEach(el => {
-		el.classList.remove("active");
-		dataFilter = el.getAttribute("data-filter")
-		if (dataFilter === currentProjectsFilter) { el.classList.add("active") }
-	})
+    projectsFilterMenu.forEach(el => {
+        el.classList.remove("active");
+        dataFilter = el.getAttribute("data-filter")
+        if (dataFilter === currentProjectsFilter) { el.classList.add("active") }
+    })
 }
 
 function updateProjectsFilter() {
-	projects.forEach(el => {
-		el.classList.remove("dimmed");
-		if (el.classList.contains(currentProjectsFilter) === false && currentProjectsFilter != "*") { el.classList.add("dimmed") };
-	})
+    projects.forEach(el => {
+        el.classList.remove("dimmed");
+        if (el.classList.contains(currentProjectsFilter) === false && currentProjectsFilter != "*") { el.classList.add("dimmed") };
+    })
+}
+
+
+
+/*------------------------------------
+    My Project Modal
+-------------------------------------- */
+
+//add click event listener to each project
+document.querySelectorAll(".open-modal-on-click").forEach(element => element.addEventListener("click", (e) => openProjectModal(e, element)));
+document.querySelectorAll(".close-modal-on-click").forEach(element => element.addEventListener("click", (e) => closeProjectModals(e)));
+
+function openProjectModal(event, projectOverlay) {
+    event.preventDefault();
+    event.stopPropagation();
+    document.getElementById("portfolio-modal").classList.remove("closed");
+
+
+    const projectId = projectOverlay.closest(".project").getAttribute("id");
+    const projectDetail = document.getElementById("project-detail-" + projectId);
+    projectDetail.classList.add("show");
+}
+
+function closeProjectModals(event) {
+    event.preventDefault();
+    document.getElementById("portfolio-modal").classList.add("closed");
+    document.querySelectorAll(".project-detail-container").forEach(projectDetail => projectDetail.classList.remove("show"));
 }
 
 
@@ -95,16 +122,9 @@ function updateProjectsFilter() {
 
 
 
-	
-
-	
 
 
 
 
-
-
-
-                
 
 
